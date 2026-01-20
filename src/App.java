0dc511a9e843ada9468f1f7778ed44e0c28b33e5
Emilio Albán <emiliofa.alban@hotmail.com>
2026-01-19 20:14:16 -0500

    Re-inicialización de Bases de Datos

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ import UserInterface.ConsolaUI;
 
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
+
         System.out.println(":)");
-=======
+
         new ConsolaUI().iniciar();
->>>>>>> 439d425c38fa3933598fa8ddd6396c234bc7466a
+
     }
 }
